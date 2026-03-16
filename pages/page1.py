e43abc558f8aa4ab8fe20ab203a16301a762e243
Lyna BOUDAMOUS <lyna.boudamous@etu.univ-tours.fr>
2026-03-16 10:54:36 +0100

    creation de la premiere page layout

---
+++ pages/page1.py
@@ -0,0 +1,8 @@
+from dash import Dash, dash_table, dcc, html
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from dash import html, dcc, dash_table, register_page
+import pandas as pd
+
